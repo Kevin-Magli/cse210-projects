5f08fd6ba0daee22fb183df24025958a251ba57e
Kevin-Magli <kmagliocchi@gmail.com>
2024-07-11 18:31:35 -0300

    Learning 05 complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>()
+        {
+            new Square(5),
+            new Rectangle(5, 4),
+            new Circle(5)
+        };
+
+        // set colors
+
+        shapes[0].SetColor("Red");
+        shapes[1].SetColor("Green");
+        shapes[2].SetColor("Blue");
+        
+        Console.WriteLine("Shape Colors:");
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+        }
+
+        Console.WriteLine("Shape Details:");
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetArea());
+        }
+
     }
 }
